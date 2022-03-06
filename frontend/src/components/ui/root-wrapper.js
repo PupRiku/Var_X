ddc73fc1e3a48bba3fd36dc89ea1fdd05ee1d37f
Chris Diorio <chris.diorio12@gmail.com>
2022-03-05 23:49:04 -0600

    Prettier updates

--- frontend/src/components/ui/root-wrapper.js
+++ frontend/src/components/ui/root-wrapper.js
@@ -3,5 +3,5 @@ import { ThemeProvider } from '@material-ui/core/styles';
 import theme from './theme';
 
 export default ({ element }) => {
-	return <ThemeProvider theme={theme}>{element}</ThemeProvider>;
+  return <ThemeProvider theme={theme}>{element}</ThemeProvider>;
 };
