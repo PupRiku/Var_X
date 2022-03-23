097b143fc7b7f3109bcbf5064bd9abf3c5c8bb36
Chris Diorio <chris.diorio12@gmail.com>
2022-03-23 09:20:32 -0500

    Hero Block implementation

--- frontend/src/components/ui/layout.js
+++ frontend/src/components/ui/layout.js
@@ -28,16 +28,8 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header categories={data.allStrapiCategory.edges} />
-      <div style={{marginBottom: '10rem'}} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-      </div>
+      <div style={{ marginBottom: '10rem' }} />
+      <main>{children}</main>
       <Footer />
     </>
   );
