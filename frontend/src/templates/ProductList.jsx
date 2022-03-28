d8cd87d9757e4867438ccfa3f7784fbc825ac034
Chris Diorio <chris.diorio12@gmail.com>
2022-03-28 18:57:27 -0500

    Go to top button

--- frontend/src/templates/ProductList.jsx
+++ frontend/src/templates/ProductList.jsx
@@ -1,11 +1,26 @@
 /* eslint-disable */
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
+import Fab from '@material-ui/core/Fab';
 import Grid from '@material-ui/core/Grid';
 import { graphql } from 'gatsby';
+import { makeStyles } from '@material-ui/core/styles';
 
 import Layout from '../components/ui/layout';
 import DynamicToolbar from '../components/product-list/DynamicToolbar';
 import ListOfProducts from '../components/product-list/ListOfProducts';
+
+const useStyles = makeStyles((theme) => ({
+  fab: {
+    alignSelf: 'flex-end',
+    marginRight: '2rem',
+    marginBottom: '2rem',
+    color: '#fff',
+    fontFamily: 'Montserrat',
+    fontSize: '5rem',
+    width: '5rem',
+    height: '5rem',
+  },
+}));
 
 export default function ProductList({
   pageContext: { filterOptions, name, description },
@@ -13,11 +28,18 @@ export default function ProductList({
     allStrapiProduct: { edges: products },
   },
 }) {
+  const classes = useStyles();
   const [layout, setLayout] = useState('grid');
+  const scrollRef = useRef(null);
+
+  const scroll = () => {
+    scrollRef.current.scrollIntoView({ behavior: 'smooth' });
+  };
 
   return (
     <Layout>
       <Grid container direction='column' alignItems='center'>
+        <div ref={scrollRef} />
         <DynamicToolbar
           filterOptions={filterOptions}
           name={name}
@@ -26,6 +48,9 @@ export default function ProductList({
           setLayout={setLayout}
         />
         <ListOfProducts layout={layout} products={products} />
+        <Fab onClick={scroll} color='primary' classes={{ root: classes.fab }}>
+          ^
+        </Fab>
       </Grid>
     </Layout>
   );
