99e6687abedc26fc8c6aef1d098de8e78bae4d93
Chris Diorio <chris.diorio12@gmail.com>
2022-04-01 13:56:50 -0500

    Setup Product info container

---
+++ frontend/src/components/product-detail/ProductInfo.jsx
@@ -0,0 +1,53 @@
+/* eslint-disable */
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  background: {
+    backgroundColor: theme.palette.secondary.main,
+    height: '45rem',
+    width: '35rem',
+  },
+  center: {
+    backgroundColor: theme.palette.primary.main,
+    height: '35rem',
+    width: '45rem',
+    position: 'absolute',
+  },
+}));
+
+export default function ProductInfo({
+  name,
+  description,
+  variants,
+  selectedVariant,
+  setSelectedVariant,
+}) {
+  const classes = useStyles();
+
+  return (
+    <Grid
+      item
+      container
+      direction='column'
+      xs={6}
+      justifyContent='center'
+      alignItems='flex-end'
+    >
+      <Grid
+        item
+        container
+        direction='column'
+        classes={{ root: classes.background }}
+      ></Grid>
+      <Grid
+        item
+        container
+        direction='column'
+        classes={{ root: classes.center }}
+      ></Grid>
+    </Grid>
+  );
+}
