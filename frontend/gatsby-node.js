f048376d645f9a9dbd7e1be88e9afe6420d8863f
Chris Diorio <chris.diorio12@gmail.com>
2022-04-01 13:27:25 -0500

    Setup product detail pages

--- frontend/gatsby-node.js
+++ frontend/gatsby-node.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions;
 
@@ -9,8 +10,19 @@ exports.createPages = async ({ graphql, actions }) => {
             node {
               name
               strapiId
+              description
               category {
                 name
+              }
+              variants {
+                id
+                color
+                size
+                style
+                price
+                images {
+                  url
+                }
               }
             }
           }
@@ -59,6 +71,8 @@ exports.createPages = async ({ graphql, actions }) => {
         name: product.node.name,
         id: product.node.strapiId,
         category: product.node.category.name,
+        description: product.node.description,
+        variants: product.node.variants,
       },
     });
   });
