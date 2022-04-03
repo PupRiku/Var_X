20eb9cc8ead8fe5b92b30828698703c14df80ace
Chris Diorio <chris.diorio12@gmail.com>
2022-04-03 17:28:10 -0500

    Setup login page

---
+++ frontend/src/pages/account.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable */
+import React from 'react';
+
+import Layout from '../components/ui/layout';
+import AuthPortal from '../components/auth/AuthPortal';
+
+export default function Account() {
+  return (
+    <Layout>
+      <AuthPortal />
+    </Layout>
+  );
+}
