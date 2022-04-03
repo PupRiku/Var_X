16f9afe31db5aa653fde945ce83c5e2372fda6b7
Chris Diorio <chris.diorio12@gmail.com>
2022-04-03 17:58:18 -0500

    Add validation for login

--- frontend/src/components/ui/validate.js
+++ frontend/src/components/ui/validate.js
@@ -1,18 +1,23 @@
+/* eslint-disable */
 export default function validate(values) {
   // ? Input: values = { field: value, field2: value }
   // ?              eg { email: chris@var-x.com, phone: 555-555-5555 }
   // ? Output: { field: valid } eg { email: true, phone: true }
   const validators = {
-    email: (val) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(val),
-    phone: (val) =>
+    email: val => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(val),
+    phone: val =>
       /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/.test(val),
-    name: (val) => val.length > 3,
-    message: (val) => val.length > 3,
+    name: val => val.length > 3,
+    message: val => val.length > 3,
+    password: val =>
+      /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/.test(
+        val
+      ),
   };
 
   const valid = {};
 
-  Object.keys(values).map((field) => {
+  Object.keys(values).map(field => {
     valid[field] = validators[field](values[field]);
   });
 
