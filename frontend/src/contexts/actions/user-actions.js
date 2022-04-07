848f50a376c4809aacf6d3816ebbe2fff01dc1bd
Chris Diorio <chris.diorio12@gmail.com>
2022-04-07 13:28:26 -0500

    cleanup code

--- frontend/src/contexts/actions/user-actions.js
+++ frontend/src/contexts/actions/user-actions.js
@@ -1,9 +1,7 @@
 /* eslint-disable */
 import { SET_USER } from './action-types';
 
-export const setUser = user => {
-  return {
-    type: SET_USER,
-    payload: { user },
-  };
-};
+export const setUser = user => ({
+  type: SET_USER,
+  payload: { user },
+});
