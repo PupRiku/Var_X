e60ce7d19c8f2f92462ca57301d42fb4c2017e25
Chris Diorio <chris.diorio12@gmail.com>
2022-04-08 10:19:19 -0500

    Undo ngrok changes

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,7 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('', 'https://fb6a-71-86-161-68.ngrok.io'),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '88fd4c3c03e61ab683295912d298997e'),
