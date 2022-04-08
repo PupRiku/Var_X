7e411543857ab32459224aebf5ca05a8fa2a0d28
Chris Diorio <chris.diorio12@gmail.com>
2022-04-08 09:11:58 -0500

    Configs for FB login

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('', 'https://776f-71-86-161-68.ngrok.io'),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '88fd4c3c03e61ab683295912d298997e'),
