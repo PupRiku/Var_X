1101a8afbeb380303791e333de63deae311dc62c
Chris Diorio <chris.diorio12@gmail.com>
2022-04-22 21:56:55 -0500

    Fix warnings

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -9,7 +9,7 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import { makeStyles } from '@material-ui/core/styles';
 
-import Fields from './fields';
+import Fields from './Fields';
 import { setUser, setSnackbar } from '../../contexts/actions';
 
 import accountIcon from '../../images/account.svg';
--- frontend/src/components/auth/Reset.jsx
+++ frontend/src/components/auth/Reset.jsx
@@ -7,7 +7,7 @@ import Button from '@material-ui/core/Button';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { makeStyles } from '@material-ui/core/styles';
 
-import Fields from './fields';
+import Fields from './Fields';
 import { EmailPassword } from './Login';
 
 import { setSnackbar } from '../../contexts/actions';
