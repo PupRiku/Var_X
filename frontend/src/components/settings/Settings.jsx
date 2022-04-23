e8da5c61f2e45bb7c39b0a50c3691ecc1190ec46
Chris Diorio <chris.diorio12@gmail.com>
2022-04-22 22:12:49 -0500

    Setup user settings

--- frontend/src/components/settings/Settings.jsx
+++ frontend/src/components/settings/Settings.jsx
@@ -4,10 +4,16 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
+import Details from './Details';
+
 const useStyles = makeStyles(theme => ({}));
 
 export default function Settings() {
   const classes = useStyles();
 
-  return <div>User Settings</div>;
+  return (
+    <Grid container>
+      <Details />
+    </Grid>
+  );
 }
