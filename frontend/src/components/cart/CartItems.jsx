ff3fa931e856380ff2bfa09bb7a7ca0ec762f0d5
Chris Diorio <chris.diorio12@gmail.com>
2022-04-25 20:42:04 -0500

    Pre-styling display of cart items

--- frontend/src/components/cart/CartItems.jsx
+++ frontend/src/components/cart/CartItems.jsx
@@ -1,13 +1,24 @@
 /* eslint-disable */
-import React from 'react';
+import React, { useContext } from 'react';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+
+import Item from './Item';
+
+import { CartContext } from '../../contexts';
 
 const useStyles = makeStyles(theme => ({}));
 
 export default function CartItems() {
   const classes = useStyles();
+  const { cart } = useContext(CartContext);
 
-  return null;
+  return (
+    <Grid item container xs={6} direction='column'>
+      {cart.map(item => (
+        <Item item={item} key={item.variant.id} />
+      ))}
+    </Grid>
+  );
 }
