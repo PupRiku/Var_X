6243adc98072905063abc53dd3608df8defe7034
Chris Diorio <chris.diorio12@gmail.com>
2022-04-28 22:07:30 -0500

    Setup contact info step

--- frontend/src/components/cart/CheckoutPortal.jsx
+++ frontend/src/components/cart/CheckoutPortal.jsx
@@ -1,10 +1,11 @@
 /* eslint-disable */
-import React from 'react';
+import React, { useState } from 'react';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
 import CheckoutNavigation from './CheckoutNavigation';
+import Details from '../settings/Details';
 
 const useStyles = makeStyles(theme => ({
   stepContainer: {
@@ -14,19 +15,56 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-export default function CheckoutPortal() {
+export default function CheckoutPortal({ user }) {
   const classes = useStyles();
+  const [selectedStep, setSelectedStep] = useState(0);
+  const [detailValues, setDetailValues] = useState({
+    name: '',
+    email: '',
+    phone: '',
+  });
+  const [detailSlot, setDetailSlot] = useState(0);
+  const [errors, setErrors] = useState({});
+
+  const steps = [
+    {
+      title: 'Contact Info',
+      component: (
+        <Details
+          user={user}
+          values={detailValues}
+          setValues={setDetailValues}
+          slot={detailSlot}
+          setSlot={setDetailSlot}
+          errors={errors}
+          setErrors={setErrors}
+          checkout
+        />
+      ),
+    },
+    { title: 'Address' },
+    { title: 'Shipping' },
+    { title: 'Payment' },
+    { title: 'Confirmation' },
+    { title: `Thanks, ${user.username}!` },
+  ];
 
   return (
     <Grid item container direction='column' xs={6} alignItems='flex-end'>
-      <CheckoutNavigation />
+      <CheckoutNavigation
+        steps={steps}
+        selectedStep={selectedStep}
+        setSelectedStep={setSelectedStep}
+      />
       <Grid
         item
         container
         direction='column'
         alignItems='center'
         classes={{ root: classes.stepContainer }}
-      ></Grid>
+      >
+        {steps[selectedStep].component}
+      </Grid>
     </Grid>
   );
 }
