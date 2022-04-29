d2a4b426595243d75c77b6f363346a9ac87c1f0d
Chris Diorio <chris.diorio12@gmail.com>
2022-04-28 22:07:19 -0500

    Setup navigation

--- frontend/src/components/cart/CheckoutNavigation.jsx
+++ frontend/src/components/cart/CheckoutNavigation.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable */
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -10,10 +11,54 @@ const useStyles = makeStyles(theme => ({
     width: '40rem',
     height: '5rem',
   },
+  back: {
+    visibility: ({ steps, selectedStep }) =>
+      selectedStep === 0 || selectedStep === steps.length - 1
+        ? 'hidden'
+        : 'visible',
+  },
+  forward: {
+    visibility: ({ steps, selectedStep }) =>
+      selectedStep >= steps.length - 2 ? 'hidden' : 'visible',
+  },
 }));
 
-export default function CheckoutNavigation() {
-  const classes = useStyles();
+export default function CheckoutNavigation({
+  steps,
+  selectedStep,
+  setSelectedStep,
+}) {
+  const classes = useStyles({ steps, selectedStep });
 
-  return <Grid item container classes={{ root: classes.navbar }}></Grid>;
+  return (
+    <Grid
+      item
+      container
+      justifyContent='center'
+      alignItems='center'
+      classes={{ root: classes.navbar }}
+    >
+      <Button
+        onClick={() => setSelectedStep(selectedStep - 1)}
+        disabled={selectedStep === 0 || selectedStep === steps.length - 1}
+      >
+        <Grid item classes={{ root: classes.back }}>
+          <Typography variant='h5'>{'<'}</Typography>
+        </Grid>
+      </Button>
+      <Grid item>
+        <Typography variant='h5'>
+          {steps[selectedStep].title.toUpperCase()}
+        </Typography>
+      </Grid>
+      <Button
+        onClick={() => setSelectedStep(selectedStep + 1)}
+        disabled={selectedStep >= steps.length - 2}
+      >
+        <Grid item classes={{ root: classes.forward }}>
+          <Typography variant='h5'>{'>'}</Typography>
+        </Grid>
+      </Button>
+    </Grid>
+  );
 }
