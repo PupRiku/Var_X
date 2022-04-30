2541f100fdc4b5ff4211ab331107efe41d4fb1a1
Chris Diorio <chris.diorio12@gmail.com>
2022-04-29 22:51:55 -0500

    Style shipping section

--- frontend/src/components/cart/Shipping.jsx
+++ frontend/src/components/cart/Shipping.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable */
 import React from 'react';
+import clsx from 'clsx';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
@@ -7,7 +8,39 @@ import { makeStyles } from '@material-ui/core/styles';
 
 import shippingIcon from '../../images/shipping.svg';
 
-const useStyles = makeStyles(theme => ({}));
+const useStyles = makeStyles(theme => ({
+  button: {
+    backgroundColor: theme.palette.secondary.main,
+    borderRadius: 15,
+    height: '10rem',
+    width: '10rem',
+    '&:hover': {
+      backgroundColor: theme.palette.secondary.light,
+    },
+  },
+  label: {
+    fontSize: '1.5rem',
+  },
+  container: {
+    height: '100%',
+  },
+  icon: {
+    marginTop: '-2rem',
+    marginBottom: '1rem',
+  },
+  price: {
+    color: '#fff',
+  },
+  selected: {
+    backgroundColor: '#fff',
+    '&:hover': {
+      backgroundColor: '#fff',
+    },
+  },
+  selectedText: {
+    color: theme.palette.secondary.main,
+  },
+}));
 
 export default function Shipping({
   shippingOptions,
@@ -17,22 +50,52 @@ export default function Shipping({
   const classes = useStyles();
 
   return (
-    <Grid item container direction='column' alignItems='center'>
+    <Grid
+      item
+      container
+      direction='column'
+      alignItems='center'
+      justifyContent='center'
+      classes={{ root: classes.container }}
+    >
       <Grid item>
-        <img src={shippingIcon} alt='shipping' />
+        <img src={shippingIcon} alt='shipping' className={classes.icon} />
       </Grid>
-      <Grid item container>
+      <Grid item container justifyContent='space-around'>
         {shippingOptions.map(option => (
           <Grid item key={option.label}>
-            <Button classes={{ root: classes.button }}>
+            <Button
+              classes={{
+                root: clsx(classes.button, {
+                  [classes.selected]: selectedShipping === option.label,
+                }),
+              }}
+              onClick={() => setSelectedShipping(option.label)}
+            >
               <Grid container direction='column'>
                 <Grid item>
-                  <Typography variant='h5' classes={{ root: classes.label }}>
+                  <Typography
+                    variant='h5'
+                    classes={{
+                      root: clsx(classes.label, {
+                        [classes.selectedText]:
+                          selectedShipping === option.label,
+                      }),
+                    }}
+                  >
                     {option.label}
                   </Typography>
                 </Grid>
                 <Grid item>
-                  <Typography variant='body1' classes={{ root: classes.price }}>
+                  <Typography
+                    variant='body1'
+                    classes={{
+                      root: clsx(classes.price, {
+                        [classes.selectedText]:
+                          selectedShipping === option.label,
+                      }),
+                    }}
+                  >
                     {`$${option.price.toFixed(2)}`}
                   </Typography>
                 </Grid>
