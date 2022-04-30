caad744da1ad140b08d4923af5d254f0401c2976
Chris Diorio <chris.diorio12@gmail.com>
2022-04-30 11:22:29 -0500

    Get all fields into the panel

--- frontend/src/components/cart/Confirmation.jsx
+++ frontend/src/components/cart/Confirmation.jsx
@@ -1,8 +1,10 @@
 /* eslint-disable */
-import React from 'react';
+import React, { useState } from 'react';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+
+import Fields from '../auth/Fields';
 
 import confirmationIcon from '../../images/tag.svg';
 import NameAdornment from '../../images/NameAdornment';
@@ -11,43 +13,160 @@ import PhoneAdornment from '../../images/PhoneAdornment';
 import streetAdornment from '../../images/street-adornment.svg';
 import zipAdornment from '../../images/zip-adornment.svg';
 import cardAdornment from '../../images/card.svg';
-import promoAdornment from '../../images/promo-adornment.svg';
+import promoAdornment from '../../images/promo-code.svg';
 
 const useStyles = makeStyles(theme => ({
   nameWrapper: {
     height: 22,
     width: 22,
   },
-  name: {
+  text: {
     fontSize: '1rem',
     color: '#fff',
+  },
+  emailWrapper: {
+    height: 17,
+    width: 22,
+  },
+  phoneWrapper: {
+    height: 25.122,
+    width: 25.173,
+  },
+  card: {
+    height: 18,
+    width: 25,
+  },
+  priceLabel: {
+    fontSize: '1.5rem',
   },
 }));
 
 export default function Confirmation() {
   const classes = useStyles();
+  const [promo, setPromo] = useState({ promo: '' });
+  const [promoError, setPromoError] = useState({});
+
+  const firstFields = [
+    {
+      value: 'Chris Diorio',
+      adornment: (
+        <div className={classes.nameWrapper}>
+          <NameAdornment color='#fff' />
+        </div>
+      ),
+    },
+    {
+      value: 'chris.diorio12@gmail.com',
+      adornment: (
+        <div className={classes.emailWrapper}>
+          <EmailAdornment color='#fff' />
+        </div>
+      ),
+    },
+    {
+      value: '555-555-5555',
+      adornment: (
+        <div className={classes.phoneWrapper}>
+          <PhoneAdornment />
+        </div>
+      ),
+    },
+    {
+      value: '1234 Example Street',
+      adornment: <img src={streetAdornment} alt='street' />,
+    },
+  ];
+
+  const secondFields = [
+    {
+      value: 'Madison, WI 53717',
+      adornment: <img src={zipAdornment} alt='city, state, zip code' />,
+    },
+    {
+      value: '**** **** **** 1234',
+      adornment: (
+        <img src={cardAdornment} alt='credit card' className={classes.card} />
+      ),
+    },
+    {
+      promo: {
+        helperText: '',
+        placeholder: 'Promo code',
+        startAdornment: <img src={promoAdornment} alt='promo code' />,
+      },
+    },
+  ];
+
+  const prices = [
+    {
+      label: 'SUBTOTAL',
+      value: 99.99,
+    },
+    {
+      label: 'SHIPPING',
+      value: 9.99,
+    },
+    {
+      label: 'TAX',
+      value: 9.67,
+    },
+  ];
+
+  const adornmentValue = (adornment, value) => (
+    <>
+      <Grid item xs={1}>
+        {adornment}
+      </Grid>
+      <Grid item xs={11}>
+        <Typography variant='body1' classes={{ root: classes.text }}>
+          {value}
+        </Typography>
+      </Grid>
+    </>
+  );
 
   return (
     <Grid item container direction='column'>
       <Grid item container>
         <Grid item container direction='column' xs={8}>
-          <Grid item container>
-            <Grid item xs={1}>
-              <div className={classes.nameWrapper}>
-                <NameAdornment color='#fff' />
-              </div>
+          {firstFields.map(field => (
+            <Grid item container key={field.value}>
+              {adornmentValue(field.adornment, field.value)}
             </Grid>
-            <Grid item xs={11}>
-              <Typography variant='body1' classes={{ root: classes.name }}>
-                Chris Diorio
-              </Typography>
-            </Grid>
-          </Grid>
+          ))}
         </Grid>
         <Grid item xs={4}>
           <img src={confirmationIcon} alt='confirmation' />
         </Grid>
       </Grid>
+      {secondFields.map((field, i) => (
+        <Grid item container key={i}>
+          <Grid item xs={6}>
+            {field.promo ? (
+              <Fields
+                fields={field}
+                errors={promoError}
+                setErrors={setPromoError}
+                values={promo}
+                setValues={setPromo}
+                isWhite
+              />
+            ) : (
+              adornmentValue(field.adornment, field.value)
+            )}
+          </Grid>
+          <Grid item container xs={6}>
+            <Grid item xs={6}>
+              <Typography variant='h5' classes={{ root: classes.priceLabel }}>
+                {prices[i].label}
+              </Typography>
+            </Grid>
+            <Grid item xs={6}>
+              <Typography variant='body2'>{prices[i].value}</Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+      ))}
     </Grid>
   );
 }
