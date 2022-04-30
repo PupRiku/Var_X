ada54577297c790a85dab063e40f66616442ac18
Chris Diorio <chris.diorio12@gmail.com>
2022-04-29 22:40:05 -0500

    Setup shipping options

--- frontend/src/components/cart/CheckoutPortal.jsx
+++ frontend/src/components/cart/CheckoutPortal.jsx
@@ -7,6 +7,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import CheckoutNavigation from './CheckoutNavigation';
 import Details from '../settings/Details';
 import Locations from '../settings/Locations';
+import Shipping from './Shipping';
 
 const useStyles = makeStyles(theme => ({
   stepContainer: {
@@ -47,6 +48,13 @@ export default function CheckoutPortal({ user }) {
 
   const [errors, setErrors] = useState({});
 
+  const [selectedShipping, setSelectedShipping] = useState(null);
+  const shippingOptions = [
+    { label: 'FREE SHIPPING', price: 0 },
+    { label: '2-DAY SHIPPING', price: 9.99 },
+    { label: 'OVERNIGHT SHIPPING', price: 29.99 },
+  ];
+
   const steps = [
     {
       title: 'Contact Info',
@@ -82,7 +90,16 @@ export default function CheckoutPortal({ user }) {
         />
       ),
     },
-    { title: 'Shipping' },
+    {
+      title: 'Shipping',
+      component: (
+        <Shipping
+          shippingOptions={shippingOptions}
+          selectedShipping={selectedShipping}
+          setSelectedShipping={setSelectedShipping}
+        />
+      ),
+    },
     { title: 'Payment' },
     { title: 'Confirmation' },
     { title: `Thanks, ${user.username}!` },
