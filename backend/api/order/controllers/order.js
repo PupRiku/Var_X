fa726571309dbd9b03d089018bd5a0d54ac9c7b6
Chris Diorio <chris.diorio12@gmail.com>
2022-05-01 13:56:55 -0500

    Setup controller for placing an order

--- backend/api/order/controllers/order.js
+++ backend/api/order/controllers/order.js
@@ -4,5 +4,94 @@
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
+const { sanitizeEntity } = require('strapi-utils');
 
-module.exports = {};
+const GUEST_ID = '626ecfc6d20004618c908d7b';
+
+module.exports = {
+  async place(ctx) {
+    const {
+      shippingAddress,
+      billingAddress,
+      shippingInfo,
+      billingInfo,
+      shippingOption,
+      subtotal,
+      tax,
+      total,
+      items,
+    } = ctx.request.body;
+
+    let orderCustomer;
+
+    if (ctx.state.user) {
+      orderCustomer = ctx.state.user.id;
+    } else {
+      orderCustomer = GUEST_ID;
+    }
+
+    let serverTotal = 0;
+    let unavailable = [];
+
+    await Promise.all(
+      items.map(async clientItem => {
+        const serverItem = await strapi.services.variant.findOne({
+          id: clientItem.variant.id,
+        });
+
+        if (serverItem.qty < clientItem.qty) {
+          unavailable.push({ id: serverItem.id, qty: serverItem.qty });
+        } else {
+          serverTotal += serverItem.price * clientItem.qty;
+
+          await strapi.services.variant.update(
+            { id: clientItem.variant.id },
+            { qty: serverItem.qty - clientItem.qty }
+          );
+        }
+      })
+    );
+
+    const shippingOptions = [
+      { label: 'FREE SHIPPING', price: 0 },
+      { label: '2-DAY SHIPPING', price: 9.99 },
+      { label: 'OVERNIGHT SHIPPING', price: 29.99 },
+    ];
+
+    const shippingValid = shippingOptions.find(
+      option =>
+        option.label === shippingOption.label &&
+        option.price === shippingOption.price
+    );
+
+    if (
+      shippingValid === undefined ||
+      (serverTotal * 1.055 + shippingValid.price).toFixed(2) !== total
+    ) {
+      ctx.send({ error: 'Invalid Cart' }, 400);
+    } else if (unavailable.length > 0) {
+      ctx.send({ unavailable }, 409);
+    } else {
+      var order = await strapi.services.order.create({
+        shippingAddress,
+        billingAddress,
+        shippingInfo,
+        billingInfo,
+        shippingOption,
+        subtotal,
+        tax,
+        total,
+        items,
+        user: orderCustomer,
+      });
+
+      order = sanitizeEntity(order, { model: strapi.models.order });
+
+      if (order.user.username === 'Guest') {
+        order.user = { username: 'Guest' };
+      }
+
+      ctx.send({ order }, 200);
+    }
+  },
+};
