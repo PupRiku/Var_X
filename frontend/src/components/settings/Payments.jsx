2b967b4baba0ec634b2d9c5dcb5303feec222912
Chris Diorio <chris.diorio12@gmail.com>
2022-05-06 15:38:26 -0500

    Payments responsive design

--- frontend/src/components/settings/Payments.jsx
+++ frontend/src/components/settings/Payments.jsx
@@ -8,6 +8,7 @@ import Switch from '@material-ui/core/Switch';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { makeStyles } from '@material-ui/core/styles';
 import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
 
@@ -22,6 +23,10 @@ const useStyles = makeStyles(theme => ({
   number: {
     color: '#fff',
     marginBottom: '5rem',
+    [theme.breakpoints.down('xs')]: {
+      marginBottom: ({ checkout }) => (checkout ? '1rem' : undefined),
+      fontSize: ({ checkout }) => (checkout ? '1.5rem' : undefined),
+    },
   },
   removeCard: {
     backgroundColor: '#fff',
@@ -30,6 +35,9 @@ const useStyles = makeStyles(theme => ({
     marginLeft: '2rem',
     '&:hover': {
       backgroundColor: '#fff',
+    },
+    [theme.breakpoints.down('xs')]: {
+      marginLeft: ({ checkout }) => (checkout ? 0 : undefined),
     },
   },
   removeCardText: {
@@ -41,7 +49,7 @@ const useStyles = makeStyles(theme => ({
   icon: {
     marginBottom: '3rem',
     [theme.breakpoints.down('xs')]: {
-      marginBottom: '1rem',
+      marginBottom: ({ checkout }) => (checkout ? '3rem' : '1rem'),
     },
   },
   paymentContainer: {
@@ -64,15 +72,27 @@ const useStyles = makeStyles(theme => ({
   switchLabel: {
     color: '#fff',
     fontWeight: 600,
+    [theme.breakpoints.down('xs')]: {
+      fontSize: '1.25rem',
+    },
   },
   form: {
     width: '75%',
     borderBottom: '2px solid #fff',
     height: '2rem',
     marginTop: '-1rem',
+    [theme.breakpoints.down('xs')]: {
+      width: '85%',
+    },
   },
   spinner: {
     marginLeft: '3rem',
+  },
+  switchItem: {
+    width: '100%',
+  },
+  numberWrapper: {
+    marginBottom: '6rem',
   },
 }));
 
@@ -91,6 +111,8 @@ export default function Payments({
   const classes = useStyles({ checkout, selectedStep, stepNumber });
   const stripe = useStripe();
   const elements = useElements();
+
+  const matchesXS = useMediaQuery(theme => theme.breakpoints.down('xs'));
 
   const [loading, setLoading] = useState(false);
 
@@ -206,7 +228,16 @@ export default function Payments({
       <Grid item>
         <img src={cardIcon} alt='payment settings' className={classes.icon} />
       </Grid>
-      <Grid item container justifyContent='center'>
+      <Grid
+        item
+        container
+        justifyContent='center'
+        classes={{
+          root: clsx({
+            [classes.numberWrapper]: checkout && matchesXS,
+          }),
+        }}
+      >
         {checkout && !card.last4 ? cardWrapper : null}
         <Grid item>
           <Typography
@@ -257,7 +288,14 @@ export default function Payments({
       >
         <Slots slot={slot} setSlot={setSlot} noLabel />
         {checkout && user.username !== 'Guest' && (
-          <Grid item>
+          <Grid
+            item
+            classes={{
+              root: clsx({
+                [classes.switchItem]: matchesXS,
+              }),
+            }}
+          >
             <FormControlLabel
               classes={{
                 root: classes.switchWrapper,
